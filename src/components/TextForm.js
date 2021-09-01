9fa199b9c4797978e95f980193929506715e44f6
“Kaushal <kpareek2592@gmail.com>
2021-09-02 01:50:04 +0530

    Improving dark mode and refactoring app component

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 
 export default function TextForm(props) {
+    var data = null;
     const [text, setText] = useState('');
 
     const handleUpClick = () => {
@@ -9,6 +10,15 @@ export default function TextForm(props) {
 
     const handleLowClick = () => {
       setText(text.toLowerCase());
+    };
+
+    const handleExtractEmailClick = () => {
+        let newText = text.match(
+          /([a-zA-Z0-9._+-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi
+        );
+      
+      data = newText;
+      setText("");
     };
 
     const handleClearTextAreaClick = () => {
@@ -21,7 +31,7 @@ export default function TextForm(props) {
 
     return (
       <>
-        <div className="container">
+        <div className="container" style={{color: props.mode === "dark" ? "white" : "#042743"}}>
           <h1>{props.heading}</h1>
           <div className="mb-3">
             <textarea
@@ -30,6 +40,7 @@ export default function TextForm(props) {
               onChange={handleOnChange}
               id="myBox"
               rows="8"
+              style={{backgroundColor: props.mode === "dark" ? "gray" : "white", color: props.mode === "dark" ? "white" : "#042743"}}
             ></textarea>
           </div>
           <button className="btn btn-primary mx-1" onClick={handleUpClick}>
@@ -44,15 +55,23 @@ export default function TextForm(props) {
           >
             Clear Text
           </button>
+          <button
+            className="btn btn-primary mx-1"
+            onClick={handleExtractEmailClick}
+          >
+            Extract Email
+          </button>
         </div>
-        <div className="container my-3">
+        <div className="container my-3" style={{color: props.mode === "dark" ? "white" : "#042743"}}>
           <h2>Your text summary</h2>
           <p>
             {text.split(" ").length} words and {text.length} characters
           </p>
           <p>{0.008 * text.split(" ").length} minutes read</p>
           <h2>Preview</h2>
-          <p>{text}</p>
+          <p>{text.length > 0 ? text : 'Enter something in textbox above to preview here'}</p>
+          <h2>Extracted Email</h2>
+          {data != null && data[0]}
         </div>
       </>
     );
